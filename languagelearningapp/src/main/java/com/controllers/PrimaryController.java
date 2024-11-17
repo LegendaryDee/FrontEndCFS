0d7b9c8257ab2897e5d61aa9881b31ea1be47db5
tristenfornes <tfornes@email.sc.edu>
2024-11-17 17:36:13 -0500

    primary controller

--- languagelearningapp/src/main/java/com/controllers/PrimaryController.java
+++ languagelearningapp/src/main/java/com/controllers/PrimaryController.java
@@ -1,15 +1,15 @@
 package com.controllers;
 
 import java.io.IOException;
-
-import com.language.App;
-
 import javafx.fxml.FXML;
+import com.language.App;
+import com.narration.*;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
+        Narrator.playSound("Hola Mundo");
         App.setRoot("secondary");
     }
 }
