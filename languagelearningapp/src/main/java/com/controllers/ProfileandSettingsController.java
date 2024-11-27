3a0665efe522ab8d1d634d72f5ce08faed09b2f9
DurantChristian <cdd7@email.sc.edu>
2024-11-27 16:53:05 -0500

    created settings

---
+++ languagelearningapp/src/main/java/com/controllers/ProfileandSettingsController.java
@@ -0,0 +1,45 @@
+package com.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+
+public class ProfileandSettingsController {
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private TextField nameField, emailField, phoneField;
+
+    public void saveProfile() {
+        String name = nameField.getText();
+        String email = emailField.getText();
+        String phone = phoneField.getText();
+
+        // Save the details (e.g., to a database or file)
+        System.out.println("Profile saved: " + name + ", " + email + ", " + phone);
+}
+
+@FXML
+private CheckBox notificationsCheckBox, darkModeCheckBox, onlineStatusCheckBox;
+
+@FXML
+private ComboBox<String> languageComboBox;
+
+public void saveSettings() {
+    boolean notifications = notificationsCheckBox.isSelected();
+    boolean darkMode = darkModeCheckBox.isSelected();
+    boolean showOnlineStatus = onlineStatusCheckBox.isSelected();
+    String language = languageComboBox.getValue();
+
+    // Save the settings (e.g., to a configuration file)
+    System.out.println("Settings saved: " + notifications + ", " + darkMode + ", " + showOnlineStatus + ", " + language);
+}
+
+@FXML
+public void initialize() {
+    // Populate the ComboBox programmatically
+    languageComboBox.getItems().addAll("English", "Spanish", "French", "German");
+}
+}
