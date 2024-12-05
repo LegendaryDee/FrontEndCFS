6315bcc07916c6f46a308a5cf9da9e3dae663aea
DurantChristian <cdd7@email.sc.edu>
2024-12-05 08:45:45 -0500

    edited user_home

--- languagelearningapp/src/main/java/com/language/App.java
+++ languagelearningapp/src/main/java/com/language/App.java
@@ -18,11 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Correct path for loading FXML file
-<<<<<<< HEAD
-        scene = new Scene(loadFXML("home"), 640, 480);
-=======
         scene = new Scene(loadFXML("numberOne"), 640, 480);
->>>>>>> 5cb95f8cb956ad399590b586650943f4b3d4a0e0
         // Set the title for the stage
         stage.setTitle("El Chicos Language Learning App");
         stage.setScene(scene);
